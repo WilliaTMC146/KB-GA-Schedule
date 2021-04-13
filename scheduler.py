8bccb4e002a27ae1304e7c00ca29c75960bf4ed8
WilliamTM <74644204+WilliaTMC146@users.noreply.github.com>
2021-04-13 23:23:42 +0800

    Revert "Add files via upload"

--- scheduler.py
+++ scheduler.py
@@ -1,39 +1,33 @@
 import numpy as np
 import random as rd
 
+#- Global Variable -#
+durasi = []
+libur = 0
 #- Class -#
 class Schedule:
+    rapat = []
+    fitness = 0
+
+    # coba2
+    total_rapat = 0
+
     def __init__(self):
-        self.rapat = []
-        self.fitness = 0
-        self.total_durasi_rapat = 0
-        self.value = 0
-        self.libur = 0
-
-        for i in range(0, 10):
-            self.rapat.append(rd.randrange(1, 8))
-
-    def fitness_function(self):
-        sum_total_rapat = 0
-        self.total_durasi_rapat = (np.sum(self.rapat) * 2)
-        for i in range(1, 8):
-            if self.rapat.count(i) == 0:
-                self.libur += 1
-            sum_total_rapat += 2**self.rapat.count(i)
-        self.value = self.total_durasi_rapat + sum_total_rapat + self.libur
-        # print(self.total_durasi_rapat)
-        # print(sum_total_rapat)
-        # print(self.libur)
-        print(self.value)
-
-    def print_rapat(self):
+        for i in range(1, 10):
+            self.rapat.append(rd.randrange(1, 7))
         print(self.rapat)
-
-    def get_fitness(self):
-        return self.fitness
-
-    def print_fitness(self):
+    
+    def fitness_function(self):
+        self.fitness = (np.sum(self.rapat) * 2) 
+        for i in range(1,7):
+            if self.rapat.count(i) == 0:
+                libur+=1
+            self.fitness += 2**self.rapat.count(i)
+        self.fitness += libur
         print(self.fitness)
+
+    def get_fitness():
+        return fitness
 
     def set_meet_hour():
         pass
@@ -44,17 +38,14 @@ def fitness_function():
 
 def rand_durasi(durasi):
     for i in range(1, 10):
-        durasi.append(rd.randrange(1, 8))
+        durasi.append(rd.randrange(1, 7))
 
 #- Main -#
-durasi = []
 pop_value = 5
+
 population = []
 
-for i in range (0, pop_value):
+for i in range (1,pop_value):
     population.append(Schedule())
 
-for i in range (0, pop_value):
-    population[i].print_rapat()
-
-population[0].fitness_function()
+population[3].fitness_function()
